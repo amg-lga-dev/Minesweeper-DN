71b13ad1c2962eddbf20ccea65f00aafa06246ba
Andrew Grossfeld <amg445@cornell.edu>
2015-12-03 17:02:45 -0500

    first commit

---
+++ MineSweeper/MineSweeperGame.swift
@@ -0,0 +1,13 @@
+//
+//  MineSweeperGame.swift
+//  MineSweeper
+//
+//  Created by Andrew Grossfeld on 12/2/15.
+//  Copyright © 2015 Andrew Grossfeld. All rights reserved.
+//
+
+import UIKit
+
+class MineSweeperGame: NSObject {
+
+}
---
+++ MineSweeper/Tile.swift
@@ -0,0 +1,21 @@
+//
+//  Tile.swift
+//  MineSweeper
+//
+//  Created by Andrew Grossfeld on 12/2/15.
+//  Copyright © 2015 Andrew Grossfeld. All rights reserved.
+//
+
+import UIKit
+
+class Tile: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
