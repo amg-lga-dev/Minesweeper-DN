7caba1b5a016e8d08da40bf1a971b943aea7880b
Andrew Grossfeld <amg445@cornell.edu>
2016-02-04 19:20:20 -0500

    fixed swipe down capability in popView

--- MineSweeper/PopViewController.swift
+++ MineSweeper/PopViewController.swift
@@ -259,7 +259,7 @@ class PopViewController: UIViewController {
     @IBAction func handlePan(sender: UIPanGestureRecognizer) {
         if sender.state == UIGestureRecognizerState.Ended {
             let velocity = sender.velocityInView(smallView)
-            if (velocity.y > 0 && abs(velocity.x) > 1) { backToVC(backButton) }
+            if (velocity.y > 0 && abs(velocity.x) < 200) { backToVC(backButton) }
             else if (velocity.x > 0 && selectedContent != "Step 1") { prevPressed(prevButton) }
             else if (velocity.x < 0 && selectedContent != "Step 5") { nextPressed(nextButton) }
             else { }
